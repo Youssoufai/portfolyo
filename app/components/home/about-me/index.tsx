3a18a1368647fe8fe708e0e7279ded720606b64d
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2026-03-24 15:01:59 +0100

    change

--- app/components/home/about-me/index.tsx
+++ app/components/home/about-me/index.tsx
@@ -3,6 +3,7 @@ const AboutMe = () => {
     const servicesBedge = ["User Experience", "Mobile App Development", "Backend Development", "Responsive Design", "Prototyping", "Devops", "UI Development", "Web Development"];
     return (
         <section>
+            {/* change */}
             <div className="container">
                 <div className="border-x border-primary/10 bg-[url('/images/about-me/about-me-bg.svg')] bg-cover bg-center bg-no-repeat">
                     <div className="flex flex-col gap-9 sm:gap-12 max-w-3xl mx-auto px-4 sm:px-7 py-11 md:py-20">
